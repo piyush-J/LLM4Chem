53b1a9d74290f055ad6e8f527febfcddbc1f29f4
Piyush Jha <piyush.jha@gatech.edu>
2024-07-27 09:43:19 -0400

    add support for multi-turn conversation

--- utils/chat_generation.py
+++ utils/chat_generation.py
@@ -7,3 +7,21 @@ def generate_chat(input_text, output_text=None, prefix_chat=None):
     if prefix_chat is not None:
         chat = prefix_chat + chat
     return chat
+
+def generate_chat_v2(conversation_list, output_text=None, prefix_chat=None):
+    # if conversation_list is a string, pass it to generate_chat
+    if isinstance(conversation_list, str):
+        return generate_chat(conversation_list, output_text, prefix_chat)
+    
+    # else from conversation_list, create a chat list alternating between user (even positions) and assistant (odd positions)
+    
+    chat = []
+    for i, content in enumerate(conversation_list):
+        if i % 2 == 0:
+            chat.append({"role": "user", "content": content})
+        else:
+            chat.append({"role": "assistant", "content": content})
+
+    if prefix_chat is not None:
+        chat = prefix_chat + chat
+    return chat
